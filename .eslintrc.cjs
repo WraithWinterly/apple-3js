6aeb1255cdee933f627f09c1d91ee57bdc5e42ff
Ayden Springer <37941646+WraithWinterly@users.noreply.github.com>
2024-05-14 15:39:56 -0400

    Update eslint warnings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,19 @@ const config = {
     "plugin:@typescript-eslint/stylistic-type-checked",
   ],
   rules: {
-    "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/array-type": "warn",
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unsafe-argument": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "warn",
+    "@typescript-eslint/no-unsafe-call": "warn",
+    "@typescript-eslint/no-unsafe-return": "warn",
+    "@typescript-eslint/ban-ts-comment": "warn",
+    "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+
+    "prefer-const": "warn",
+    "@typescript-eslint/no-floating-promises": "warn",
+
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/consistent-type-imports": [
       "warn",
